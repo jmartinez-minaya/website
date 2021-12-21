a4551809606d89813f8bb210cd658bd65c222d98
Joaquin Martinez-Minaya <jomarminaya@gmail.com>
2021-12-21 12:11:24 +0100

    updating title of the initial webpage

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:C:\Users\jomar\OneDrive\Escritorio\website2\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\jomar\GITHUB\website\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
